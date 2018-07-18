db8e2db203484eab386141aa46838fca5df31074
ladislavbili <l.bilisics@gmail.com>
2018-07-18 10:52:00 +0200

    LB - 18.7

--- src/components/account/index.js
+++ src/components/account/index.js
@@ -1,103 +1,32 @@
-
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from 'react-redux';
-import { Input, Item, Container, Header, Title, Content, Button, Icon, Text, Left, Right, Body, List, ListItem, View, CheckBox, Label } from 'native-base';
-import { Actions } from 'react-native-router-flux';
+import { ActivityIndicator } from 'react-native';
+import {setUserLoading ,getUser } from '../../redux/actions';
+import Account from './account';
 import jwt_decode from 'jwt-decode';
-import {logoutUser} from '../../redux/actions';
-import I18n from '../../translations/';
 
-/**
- * Decodes current JWT Token and displays data about the current user, also allowing him to log out
- * @extends Component
- */
-class Account extends Component {
-  constructor(props) {
+class EditUserLoader extends Component {
+  constructor(props){
     super(props);
-    user=jwt_decode(this.props.token);
-
-    this.state = {
-      name:user.name,
-      surname:user.surname,
-      email:user.email,
-      user_role:user.userRoleTitle,
-      username:user.username,
-    };
+    this.props.setUserLoading(false);
+    this.props.getUser(jwt_decode(this.props.token).id,this.props.token);
   }
 
-  render() {
-    return (
-      <Container>
-        <Header>
-          <Left>
-            <Button transparent onPress={() => Actions.pop()}>
-              <Icon name="arrow-back" />
-            </Button>
-          </Left>
-          <Body>
-            <Title>{I18n.t('account')}</Title>
-          </Body>
-        </Header>
-        <Content style={{ padding: 15 }}>
-
-          <Text note>{I18n.t('username')}</Text>
-          <View style={{ borderColor: '#CCCCCC', borderWidth: 0.5, marginBottom: 5 }}>
-            <Input
-              value={this.state.username}
-              disabled={true}
-              />
-          </View>
-
-          <Text note>{I18n.t('firstName')}</Text>
-          <View style={{ borderColor: '#CCCCCC', borderWidth: 0.5, marginBottom: 5 }}>
-            <Input
-              value={this.state.name}
-              disabled={true}
-              />
-          </View>
-
-          <Text note>{I18n.t('surname')}</Text>
-          <View style={{ borderColor: '#CCCCCC', borderWidth: 0.5, marginBottom: 5 }}>
-            <Input
-              value={this.state.surname}
-              disabled={true}
-              />
-          </View>
-
-          <Text note>{I18n.t('email')}</Text>
-          <View style={{ borderColor: '#CCCCCC', borderWidth: 0.5, marginBottom: 5 }}>
-            <Input
-              value={this.state.email}
-              disabled={true}
-              />
-          </View>
-
-          <Text note>{I18n.t('userRole')}</Text>
-          <View style={{ borderColor: '#CCCCCC', borderWidth: 0.5, marginBottom: 5 }}>
-            <Input
-              value={this.state.user_role}
-              disabled={true}
-              />
-          </View>
-
-          <Button danger block onPress={()=>{this.props.logoutUser();Actions.pop();Actions.pop();Actions.pop();Actions.pop();}}
-            iconLeft style={{ flexDirection: 'row', borderColor: 'white', marginTop:5, marginBottom:20, borderWidth: 0.5 }}>
-            <Icon active style={{ color: 'white' }} name="power" />
-            <Text style={{ color: 'white' }} >{I18n.t('logout')}</Text>
-          </Button>
-
-
-        </Content>
-      </Container>
-    );
+  render(){
+    if(!this.props.userLoaded){
+      return <ActivityIndicator
+        animating size={ 'large' }
+        color='#007299' />
+    }
+    return <Account {...this.props}/>
   }
 }
 
-//creates function that maps actions (functions) to the redux store
-const mapStateToProps = ({ login }) => {
-  const { token } = login;
-  return { token };
+//all below is just redux storage
+const mapStateToProps = ({ loginReducer, userReducer }) => {
+  const { userLoaded} = userReducer;
+  const {token} = loginReducer;
+  return { userLoaded, token};
 };
 
-//exports created Component connected to the redux store and redux actions
-export default connect(mapStateToProps, {logoutUser})(Account);
+export default connect(mapStateToProps, {setUserLoading ,getUser})(EditUserLoader);
--- src/components/settings/index.js
+++ src/components/settings/index.js
@@ -69,6 +69,7 @@ class Settings extends Component {
             <Text style={{ color: 'white' }} >{I18n.t('logout')}</Text>
           </Button>
         </Content>
+        { this.props.ACL.includes('user_settings') || this.props.ACL.includes('company_settings') &&
         <Footer>
           { this.props.ACL.includes('user_settings') &&
             <FooterTab>
@@ -87,6 +88,7 @@ class Settings extends Component {
             </FooterTab>
           }
         </Footer>
+      }
       </Container>
     );
   }
